1a3ad7c2b2793cc4ec1eeb4b5df92c574dc950b5
lukaszrybicki8 <lukaszrybicki8@interia.eu>
2017-03-04 12:58:21 +0100

    new

---
+++ src/com/sdajava/zad1/Main.java
@@ -0,0 +1,25 @@
+package com.sdajava.zad1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Czesc");
+
+        int a = 17;
+        int b = 4;
+        int c = a+b;
+
+        System.out.println("Wynik: " + c + "koniec wyniku");
+
+        a = 9;
+        b = -5;
+        c = a-b;
+        System.out.println("Wynik: " + c + "koniec wyniku");
+
+        long k = 2
+
+
+    }
+}
